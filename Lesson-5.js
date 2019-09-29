59c4af8b1e261297b1047562808621e38c7002f1
egorbarisovi4 <egorbarisovi4@gmail.com>
2019-09-30 01:34:17 +0500

    Сделать вывод

--- Lesson-5.js
+++ Lesson-5.js
@@ -1,6 +1,21 @@
 "use strict";
 
 let TAX_RATE = 0.73;
+let summ = [];
 
-console.log(100 * TAX_RATE);
-// sum * 0.73;
+for (let count = 0; count < 10; count ++) {
+    summ.push(function(costOfProd){
+        costOfProd * TAX_RATE; 
+    });
+}
+// function getTaxRate(costOfProd) {
+    // summ.push(function(costOfProd) {
+    //     return (costOfProd * TAX_RATE);
+    // }); 
+    // summ.push(); 
+    // return summ;
+    // * TAX_RATE 
+    // costOfProd * TAX_RATE;
+// }
+
+console.log(summ[0](100));
